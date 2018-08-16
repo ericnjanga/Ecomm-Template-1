848fdcc49f432796e77938b013faf2233d259fce
Eric Njanga <eric@BigMac.local>
2018-08-16 16:50:51 -0400

    code cleanup

--- src/utilities/hoc/withActiveProp.js
+++ src/utilities/hoc/withActiveProp.js
@@ -5,19 +5,25 @@ import getCompDisplayName from './../func/getCompDisplayName.js';
 /**
  * Only return the component if it's "active" property has true for value
  * @param {*} Component 
+ * 
+ * Example implementation: const Comp = withActiveProp(Component)();
  */
 export const withActiveProp = Component => props => {
   
-  const WithActiveProp = props => {
+  // Function which:
+  // - Helps name the High Order Component
+  // - Attaches the decorating functionality
+  // - Truely returns the component
+  const NameWrapper = props => {
+
+    NameWrapper.displayName = `WithActiveProp(${getCompDisplayName(Component)})`;
     
     if(!props.active) {
       return false;
     }
     
-    WithActiveProp.displayName = `WithActiveProp(${getCompDisplayName(Component)})`;
-  
     return <Component {...props} />;
   };
 
-  return WithActiveProp;
+  return NameWrapper;
 };
