74ae31448e096a8788444c699a4ccbb742b8cd55
Eric Njanga <eric@BigMac.local>
2018-08-16 13:47:39 -0400

    Setting up landing screens

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { tempData } from './settings/temp-data.js';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      screens: [...tempData.screens],
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {
+          this.state.screens.map(screen => {
+            return (
+              <div className="screen">
+                <p>screen 1</p>
+              </div>
+            )
+          })
+        }
+
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
---
+++ src/settings/temp-data.js
@@ -0,0 +1,29 @@
+
+export const tempData = {
+  screens: [
+    {
+      name: 'login',
+      title: 'Login',
+      info1: 'info1',
+      info2: 'info2',
+    },
+    {
+      name: 'login',
+      title: 'Login',
+      info1: 'info1',
+      info2: 'info2',
+    },
+    {
+      name: 'login',
+      title: 'Login',
+      info1: 'info1',
+      info2: 'info2',
+    },
+    {
+      name: 'login',
+      title: 'Login',
+      info1: 'info1',
+      info2: 'info2',
+    },
+  ],
+};
