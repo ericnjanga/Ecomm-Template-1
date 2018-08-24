301edb9fc0df67615df2c4b15d45872e45890d15
Eric Njanga <eric@BigMac.local>
2018-08-24 17:12:26 -0400

    improved top navigation

--- src/terminals/TopNavigation.js
+++ src/terminals/TopNavigation.js
@@ -4,6 +4,21 @@ import BrandLogo from './BrandLogo.js';
 const TopNavigation = () => (
   <div className="top-navigation">
     <BrandLogo />
+
+    <ul className="list-inline">
+      <li className="list-inline-item">
+        <a href="#">Auth</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">Home</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">Single</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">Admin</a>
+      </li>
+    </ul>
   </div>
 );
 
