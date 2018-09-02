cbac8b475b88a6c9026c721ab410803340917986
Eric Njanga <njanga.webdev@gmail.com>
2018-09-02 11:09:44 -0400

    Successfully replaced basic navigation with reactstrap navigation

--- src/terminals/TopNavigation.js
+++ src/terminals/TopNavigation.js
@@ -1,30 +1,98 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import BrandLogo from './BrandLogo.js';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const TopNavigation = () => {
+export default class TopNavigation extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="top-navigation">
-      <BrandLogo />
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+
+
+
+// const TopNavigation = () => {
+
+//   return (
+//     <div className="top-navigation">
+//       <BrandLogo />
   
-      <ul className="list-inline">
-        <li className="list-inline-item">
-          <NavLink to={`/items`}>Home</NavLink>
-        </li>
-        <li className="list-inline-item">
-          <NavLink to={`/`}>Single</NavLink>
-        </li>
-        <li className="list-inline-item">
-          <NavLink to={`/admin`}>Admin</NavLink>
-        </li>
-      </ul>
-    </div>
-  );
+//       <ul className="list-inline">
+//         <li className="list-inline-item">
+//           <NavLink to={`/items`}>Home</NavLink>
+//         </li>
+//         <li className="list-inline-item">
+//           <NavLink to={`/`}>Single</NavLink>
+//         </li>
+//         <li className="list-inline-item">
+//           <NavLink to={`/admin`}>Admin</NavLink>
+//         </li>
+//       </ul>
+//     </div>
+//   );
 
-};
-
-
+// };
 
 
-export default TopNavigation;
+
+
+// export default TopNavigation;
