389150a10db6846d9d456c11e9117b93e3649abc
Eric Njanga <njanga.webdev@gmail.com>
2018-09-05 10:10:57 -0400

    Imprived layout top spacing and navbar settings

--- src/terminals/TopNavigation.js
+++ src/terminals/TopNavigation.js
@@ -31,7 +31,12 @@ export default class TopNavigation extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light expand="md">
+        <Navbar
+          color="light"
+          light
+          fixed="top"
+          expand="md"
+        >
           <NavbarBrand href="/">reactstrap</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
