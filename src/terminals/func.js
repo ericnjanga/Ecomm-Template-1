0b6aec659f4518e28ec3f2022179e527101b5143
Eric Njanga <njanga.webdev@gmail.com>
2018-09-11 18:53:54 -0400

    [update record] reorganising the "resetformdata" function

--- src/terminals/func.js
+++ src/terminals/func.js
@@ -5,16 +5,28 @@
  * Reset form properties connected to the state
  * @param {*} dataNode 
  */
+
+
+import { appStructure } from './../settings/app-structure.js';
+
+/**
+ * Returns initial data for each item
+ * @param {*} dataNode 
+ */
 export function resetStateForms (dataNode) {
-  // for presets ...
-  if (dataNode==='presets') {
-    const { screens } = this.state;
 
-    if (dataNode) {
+  const { screens } = this.state;
+  // console.log('>>>dataNode=', dataNode);
+
+  switch(dataNode){
+    case 'presets':
       screens[2].sections[1].items[0].formData = { name: '' };
-      this.setState({ screens });
-    }
-
+      break;
+    case 'product':
+      screens[2].sections[2].items[0].formData = appStructure.screens[2].sections[2].items[0].formData;
+      break;
   }
+
+  this.setState({ screens });
 
 }; //[end] resetStateForms
