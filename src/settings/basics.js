dc693464a9834e5a308cfd9a446421f804efaba0
Eric Njanga <njanga.webdev@gmail.com>
2018-09-15 08:04:30 -0400

    [release phase 1] defining production environment

--- src/settings/basics.js
+++ src/settings/basics.js
@@ -11,7 +11,7 @@ import React from 'react';
 import firebase, { firebaseConfig } from './firebase-configs';
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig.dev);
+firebase.initializeApp(firebaseConfig.prod); // env or prod
 
 // Application prefix
 export const APP_PREFIX = 'Et1';
--- src/settings/firebase-configs.js
+++ src/settings/firebase-configs.js
@@ -4,6 +4,8 @@
 
 import firebase from 'firebase';
 
+// const env
+
 export const firebaseConfig = {
   dev: {
     apiKey: 'AIzaSyC3IUTe2DZfCGVO2dT5ekSuzD2sftiCClM',
@@ -11,6 +13,14 @@ export const firebaseConfig = {
     databaseURL: 'https://ecomm-template1.firebaseio.com',
     projectId: 'ecomm-template1',
     storageBucket: 'ecomm-template1.appspot.com',
+    messagingSenderId: '25676979886',
+  },
+  prod: {
+    apiKey: 'AIzaSyBOAG43skMxFHF2zKEiN4VGoeFZFmD-FX0',
+    authDomain: 'dealerks-e9540.firebaseapp.com',
+    databaseURL: 'https://dealerks-e9540.firebaseio.com',
+    projectId: 'dealerks-e9540',
+    storageBucket: 'dealerks-e9540.appspot.com',
     messagingSenderId: '25676979886',
   },
 };
